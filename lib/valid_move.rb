5f223612bad7091260b634d73378f32bb9e6f69d
Ofor Iman <nnajim17@mytru.ca>
2019-10-03 21:48:33 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,21 @@
 # code your #valid_move? method here
+def position_taken?(board, index)
+  if board[index] == " " || board[index] =="" ||  board[index] ==nil
+    false
+  elsif board[index] == "X" || board[index]= "O"
+    true
+  end
+end
+
+def valid_move(index)
+  upperrangeofpossiblities= 8
+  lowerrangeofpossiblities= 0
+  if index>= upperrangeofpossiblities && index<= upperrangeofpossiblities
+    position_taken?(board,index)
+  else
+    puts"Enter valid number!"
+  end
+end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
